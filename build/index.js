660eae83a31d12a430e351efd86047363350ea93
Dallas <dallasjt31@gmail.com>
2023-02-16 21:09:26 +1100

    updated files

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hello = void 0;
-const world = 'world1';
+const world = 'world';
 function hello(who = world) {
     return `Hello ${who}! `;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const world = 'world1';
+const world = 'world';
 
 export function hello(who: string = world): string {
   return `Hello ${who}! `;
